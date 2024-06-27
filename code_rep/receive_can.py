e05a2c64c0ee1074b894b958789d7615de4f5a0f
Akira-Al <tonozukaalexanderakira@gmail.com>
2024-06-27 11:29:03 +0900

    implemented health checker

--- code_rep/receive_can.py
+++ code_rep/receive_can.py
@@ -1,21 +1,46 @@
 import can
 import subprocess
 import json
+import time
 
+# load config file
 config_file = open("code_rep/config.json")
 config = json.load(config_file)
+config_file.close()
 
 can_list = config["can_channel"]
 bus_list = []
 log_file_path = "log/log.txt"
-health_check = dict()
+
 # seconds
 can_send_period = 5
 check_can_received = 10
+# last health checked timestamp
+last_health_check = dict()
+for header in config["check_validation"].keys():
+    last_health_check[header] = dict()
+    # store [timestamp, bool]
+
+timestamp_on_start = time.time()
+
+
+# recieve arbitration_id turned into hex str, call by f(hex(arbitration_id))
+def get_header(arbitration_id: str) -> str:
+    return arbitration_id[0 : len(arbitration_id) - 2]
+
+
+def get_tail(arbitration_id: str) -> str:
+    l = len(arbitration_id)
+    return arbitration_id[l - 2 : l]
+
+
+def get_gachacon_id(arbitration_id: str) -> int:
+    return int(get_tail(arbitration_id))
 
 
 class CallBackFunction(can.Listener):
     def on_message_received(self, msg: can.Message) -> None:
+        # data format for stdout and log
         id_str = hex(msg.arbitration_id)
         id_str = id_str.replace("0x", "")
         id_str = "0" * (8 - len(id_str)) + id_str
@@ -30,16 +55,25 @@ class CallBackFunction(can.Listener):
             + "#"
             + msg.data.hex()
         )
+        # send data to stdout
         print(can_info_str)
+        # store data in log
         log_file = open(log_file_path, "a")
         log_file.write(can_info_str + "\n")
         log_file.close()
+
+        # check if information for health chk
+        check_helth_list = config["check_validation"]
+        id_header = get_header(hex(msg.arbitration_id))
+        if id_header in check_helth_list.keys():
+            gachacon_id = get_gachacon_id(hex(msg.arbitration_id))
+            last_health_check[id_header][gachacon_id] = [time.time(), msg.data.hex()]
 
     def stop(self) -> None:
         return super().stop()
 
 
-send_check_range = config["send_check_valid"]["range"]
+send_check_range = config["range"]
 msgs = []
 for i in range(send_check_range["start"], send_check_range["end"]):
     id_tail = hex(i)[2:]
@@ -70,7 +104,20 @@ for st in can_list:
         bus_tmp.send_periodic(msgs=msg, period=can_send_period)
 try:
     while True:
-        pass
+        time.sleep(check_can_received)
+        check_valid_list = config["check_validation"]
+        st = config["range"]["start"]
+        end = config["range"]["end"]
+        for id_header in check_valid_list.keys():
+            for gachacon_id in range(st, end):
+                health_state = last_health_check[id_header][gachacon_id]
+                if not (
+                    abs(health_state[0] - check_can_received)
+                    and health_state[1] == check_valid_list[id_header]["True"]
+                ):
+                    print(
+                        f"({time.time()}) unsufficient health state for header: {id_header}, last timestamp:{health_state[0]}, recieved health_state:{health_state[1]}"
+                    )
 except KeyboardInterrupt:
     print("exit")
     for i in bus_list:
